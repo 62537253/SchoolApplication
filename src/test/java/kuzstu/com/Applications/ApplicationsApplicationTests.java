fea76f06e56eea42997280ca5b75d81ed426c1f6
62537253 <144789858+62537253@users.noreply.github.com>
2023-12-19 01:33:51 +0700

    First commit

---
+++ src/test/java/kuzstu/com/Applications/ApplicationsApplicationTests.java
@@ -0,0 +1,13 @@
+package kuzstu.com.Applications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
